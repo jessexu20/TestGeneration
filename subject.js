01d0ad8095b20e04084c5d3106d6fb99377bb5bc
Jesse <jesse.xu.20@gmail.com>
2015-03-07 17:15:09 -0500

    test7

--- subject.js
+++ subject.js
@@ -2,7 +2,7 @@ var fs = require("fs");
 function inc(p, q){
     if(q ==null) q =1;
 
-   if( p < 0 )
+   if( p < -100 )
    {
    	p = -p;
    }
--- test.sh
+++ test.sh
@@ -1,16 +1,20 @@
-clear
-echo "the script start now"
-node_modules/.bin/istanbul cover test.js > test.txt
-# while read line
-# do
-# 	# echo $line
-# 	temp1=$( grep -o "[[:digit:]]{1,2}\.[[:digit:]]{1,2}[%]" <<<"$line")
+#!/bin/sh
+# Refuse to commit files with the string NOCOMMIT, debugger, or merge markers present.
 #
-# 	echo $temp1
-# done <test.txt
+echo "Check Commit Script start now"
+echo "--------Running Istanbul"
+node_modules/.bin/istanbul cover test.js > test.txt
 filename='test.txt'
-filename1='b'
-sed -n '3,6p' "$filename">b
+filename1='result.txt'
+sed -n '3,6p' "$filename">$filename1
+echo "Istanbul Finished"
+echo
+echo "--------Running Static Analysis tool Jshint!!"
+jshint subject.js
+
+echo
+echo "---------Checking whether coverage Satisfy Requirements~~~~~~"
+echo 
 while read line
 do
 var="50"
@@ -21,15 +25,19 @@ if [ "$t" == "$hundred" ];
 then
 	t="99."
 fi
+
+
 t1=${t:0:2}
 if [ "$t1" -gt "$var" ];
 then
-	echo "greater"
+	echo ${temp:0:10} "Coverage is higher than " $var "%! Pass!"
 else 
-	echo "smaller"
+	echo ${temp:0:10} "Coverage is less than " $var "%."
+	echo "!!!Failed to commit!!! Please check the test.txt for references" 
+	exit 1
 fi
 
-done < "$filename1"
-echo "end!"
+done < $filename1
 echo
+
 
